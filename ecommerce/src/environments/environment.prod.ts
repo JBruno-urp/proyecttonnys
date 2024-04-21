f75aa14668d38b5af660c9fcb98c29eafceeb3f8
Brunopdtd <biparraguirre@prediqtdata.com>
2024-04-21 11:25:13 -0500

    PROGRA

---
+++ ecommerce/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  URL_BACKEND: 'http://tonnys_style.pe/',
+  URL_SERVICIOS: 'http://api.tonnys_style.site/api/',
+  URL_FRONTED: 'http://tienda.tonnys_style.pe/',
+};
