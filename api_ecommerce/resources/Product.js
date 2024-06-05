4207b38e1df33cf771c5f58867385bbe407088d5
sebastia16 <132413526+sebastia16@users.noreply.github.com>
2024-06-05 00:41:51 +0000

    avance products

--- api_ecommerce/resources/Product.js
+++ api_ecommerce/resources/Product.js
@@ -1,4 +1,4 @@
-export default {
+/*export default {
     product_list: (product,variedades = [],avg_review = 0,count_review = 0,CampaingDiscount = null) => {
         var IMAGEN_TWO = "";
         let GALERIAS = product.galerias.map((galeria) => {
@@ -29,5 +29,24 @@ export default {
             count_review: count_review,
             campaing_discount: CampaingDiscount,
         }
+    }
+}
+*/
+export default (product) => {
+    return {
+        _id: product._id,
+        title: product.title,
+        sku: product.sku,
+        slug: product.slug,
+        imagen: 'http://localhost:3000'+'/api/products/uploads/product/'+product.portada,// 
+        categorie: product.categorie,
+        price_soles: product.price_soles,
+        price_usd: product.price_usd,
+        stock: product.stock,
+        description: product.description,
+        resumen: product.resumen,
+        tags: product.tags ? JSON.parse(product.tags) : [],
+        type_inventario: product.type_inventario,
+        state: product.state,
     }
 }
